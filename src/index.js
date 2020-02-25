832c94fe3c1fac83ade70328712fdac689e1e641
Vlad Samoylov <samoylov.dev@gmail.com>
2020-02-26 01:38:55 +0300

    move index.js to src folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 const toJson = response => response.json()
 
 const url = encodeURI(
-    'http://newsapi.org/v2/everything?sources=Lenta&qInTitle="+умер"&sortBy=publishedAt&apiKey=SECRET'
+    `http://newsapi.org/v2/everything?sources=Lenta&qInTitle="+умер"&sortBy=publishedAt&apiKey=${process.env.NEWS_TOKEN}`
 )
 
 fetch(url)
