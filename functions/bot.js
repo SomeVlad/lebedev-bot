d632b9b79f8cc3c8aa1f1cc8cc98cd4dbd471b3e
Vlad Samoylov <samoylov.dev@gmail.com>
2020-02-26 03:49:26 +0300

    try this also

--- functions/bot.js
+++ functions/bot.js
@@ -1,8 +1,8 @@
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api')
 
-export const handler = () => {
-    const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true })
+exports.handler = () => {
+    const bot = new TelegramBot(process.env.TELEGRAM_TOKEN)
 
     bot.onText(/\/start/, (msg) => {
         bot.sendMessage(msg.chat.id, 'hey there')
