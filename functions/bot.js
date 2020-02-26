5130d0ec807be6a21ef629d56373cbbacb01d5c1
Vlad Samoylov <samoylov.dev@gmail.com>
2020-02-26 03:03:22 +0300

    fix webhook

--- functions/bot.js
+++ functions/bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api')
 
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN)
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true })
 
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'hey there')
